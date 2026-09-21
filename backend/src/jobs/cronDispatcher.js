6874904ba42242c1a2c4e455b0da2398cdc985f6
Dhruv-0101 <d7511162@gmail.com>
2026-09-21 14:54:34 +0530

    Revert "module completed"
    This reverts commit 779acf1479ab7ac81c915d927d533222978bbfd6.

---
+++ backend/src/jobs/cronDispatcher.js
@@ -0,0 +1,5 @@
+/**
+ * ⏰ CRON DISPATCHER JOB (Guide Path Compatibility Export)
+ * Re-exports 1-minute Cron Dispatcher ticker from src/jobs/cron/postCron.job.js
+ */
+export * from './cron/postCron.job.js';
---
+++ backend/src/jobs/email.job.js
@@ -0,0 +1,5 @@
+/**
+ * 🛠️ BULLMQ JOB WORKER (Guide Path Compatibility Export)
+ * Re-exports BullMQ Email Worker consumer from src/jobs/workers/email.worker.js
+ */
+export * from './workers/email.worker.js';
